8f82a1479466cdc21aff1cbc4034375c8d373d5f
harry <snh90100@gmail.com>
2019-09-24 17:50:47 +0800

    add day11

---
+++ day11-tree.js
@@ -0,0 +1,44 @@
+class TreeNode {
+  constructor(data, left = null, right = null) {
+    this.data = data
+    this.left = left
+    this.right = right
+  }
+}
+
+class Tree {
+  constructor() {
+    this.root = null
+  }
+
+  // collect() {
+  //   return this._collect(this.root, []) // 
+  // }
+
+  // 使用遞迴去遍歷樹節點
+  collect(current = this.root, nodes = []) {
+    if (current === null) {
+      return nodes
+    }
+    nodes.push(current.data)
+
+    this.collect(current.left, nodes)
+    this.collect(current.right, nodes)
+    return nodes
+  }
+}
+
+let n1 = new TreeNode(50);
+let n2 = new TreeNode(43);
+let n3 = new TreeNode(67);
+let n4 = new TreeNode(81);
+let n5 = new TreeNode(75);
+
+let tree = new Tree();
+tree.root = n1
+n1.left = n2
+n1.right = n3
+n3.right = n4
+n4.left = n5
+
+console.log(tree.collect())
