2964e09f00a1330842334be1efdf57413878e198
a90100 <snh90100@gmail.com>
2019-10-04 22:55:17 +0800

    add day23

--- day22-linear-search.js
+++ day22-linear-search.js
@@ -1,6 +1,13 @@
-function linearSearch(data ,key) {
-
+function linearSearch(data, key) {
+  let index = 0;
+  while (index < data.length) {
+    if (data[index] == key) { // 某元素值和key查找值相同，回傳目前索引
+      return index;
+    }
+    index++;
+  }
+  return -1;
 }
 
-linearSearch([1, 234, -5, 78, -182, 45], 45);
-linearSearch([1, 234, -5, 78, -182, 45], 4);
+console.log(linearSearch([1, 234, -5, 78, -182, 45], 45));
+console.log(linearSearch([1, 234, -5, 78, -182, 45], 4));
---
+++ day23-binary-search.js
@@ -0,0 +1,6 @@
+function binarySearch(data, key) {
+  
+}
+
+console.log(binarySearch([1, 234, -5, 78, -182, 45], 45));
+console.log(binarySearch([1, 234, -5, 78, -182, 45], 4));
