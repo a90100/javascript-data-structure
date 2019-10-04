8ca58b0439f3bb9ea1854f14ce764c474e1482ae
a90100 <snh90100@gmail.com>
2019-10-05 01:15:57 +0800

    add day23

--- day23-binary-search.js
+++ day23-binary-search.js
@@ -1,6 +1,19 @@
 function binarySearch(data, key) {
-  
+  let left = 0;
+  let right = data.length -1;
+  let middle;
+  while(left <= right) {
+    middle = Math.floor((left + right) / 2);
+    if(data[middle] == key) {
+      return middle;
+    } else if(key > data[middle]) { // 尋找值比中間值大時，讓中間值後的一個元素位置指定left
+      left = middle + 1;
+    } else {
+      right = middle - 1;
+    }
+  }
+  return -1;
 }
 
-console.log(binarySearch([1, 234, -5, 78, -182, 45], 45));
-console.log(binarySearch([1, 234, -5, 78, -182, 45], 4));
+console.log(binarySearch([-182, -5, 1, 45, 78, 234], 45));
+console.log(binarySearch([-182, -5, 1, 45, 78, 234], 4));
